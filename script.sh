54dc5d42da4703bd52335b47c97ab205bd2bddfa
DESKTOP-BBH2SST\USER <bangjiwoojiwoo@gmail.com>
2026-09-16 09:06:15 +0900

    script_update.sh

--- script.sh
+++ script.sh
@@ -13,6 +13,6 @@ docker ps -q -f name=calculator | grep -q . && sudo docker stop calculator
 echo "이미지 $IMAGE_NAME:$TAG 다운로드 중..."
 docker pull "$IMAGE_NAME:$TAG"
 
-docker run -d -p 외부포트:내부포트 --rm --name calculator "$IMAGE_NAME:$TAG"
+docker run -d -p 9000:9000 --rm --name calculator "$IMAGE_NAME:$TAG"
 docker image prune -f
 echo "작업 완료!!!"
